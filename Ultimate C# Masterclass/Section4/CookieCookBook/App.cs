5e8755e4ffa18989b15155abab1cc4fe9346dc06
Papiśko <kkowalski0477@gmail.com>
2025-10-29 00:45:27 +0100

    Started working on CookieCookBook console app.

---
+++ Ultimate C# Masterclass/Section4/CookieCookBook/App.cs
@@ -0,0 +1,17 @@
+﻿using CookieCookBook;
+
+List<Recipe> recipes = new List<Recipe>();
+
+if (recipes.Count > 0)
+{
+    // end goal reading and displaing stuff from the text file onto the console
+
+    Console.WriteLine("Printing existing recipes.");
+
+    foreach (var recipe in recipes)
+    {
+        recipe.DisplayIngredients();
+    }
+}
+
+Console.WriteLine("Create a new cookie recipe! Available ingredients are:");
---
+++ Ultimate C# Masterclass/Section4/CookieCookBook/FileManager.cs
@@ -0,0 +1,28 @@
+﻿namespace CookieCookBook
+{
+    public static class FileManager
+    {
+        public static void CreateFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("The file has already been created. New file won't be made.");
+                return;
+            }
+
+            File.Create(filePath);
+        }
+
+        public static void ReadFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"The file with given file path {filePath} does not exist. No file has been read.");
+                return;
+            }
+
+            File.OpenRead(filePath);
+            File.ReadAllText(filePath);
+        }
+    }
+}
