4e9638854e498b49256a7d8b35b4cd40f4140abd
Papiśko <kkowalski0477@gmail.com>
2025-10-29 00:45:27 +0100

    Started working on CookieCookBook console app.

---
+++ Ultimate C# Masterclass/Section4/CookieCookBook/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿namespace CookieCookBook
+{
+    public class Ingredient
+    {
+        private int ID { get; set; }
+
+        private string Name { get; set; }
+
+        private string Instructions { get; set; }
+    }
+}
---
+++ Ultimate C# Masterclass/Section4/CookieCookBook/Recipe.cs
@@ -0,0 +1,15 @@
+﻿namespace CookieCookBook
+{
+    internal class Recipe
+    {
+        private List<Ingredient> _ingredients;
+
+        public void DisplayIngredients()
+        {
+            foreach (var ingredient in _ingredients)
+            {
+                Console.WriteLine(ingredient);
+            }
+        }
+    }
+}
