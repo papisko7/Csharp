43741be771079597122b72bd4273aa627a943ef5
Kacper Kowalski <kkowalski0477@gmail.com>
2025-11-18 22:12:41 +0100

    Created a template for a Cookie Book Recipe project.
    Created a template for a Cookie Book Recipe project.

---
+++ Ultimate C# Masterclass/Section4/CookieCookBook/RecipesConsoleUserInteraction.cs
@@ -0,0 +1,16 @@
+﻿namespace CookieCookBook
+{
+	public class RecipesConsoleUserInteraction : IRecipesUserInteraction
+	{
+		public void Exit()
+		{
+			Console.WriteLine("Press any key to close.");
+			Console.ReadKey();
+		}
+
+		public void ShowMessage(string message)
+		{
+			Console.WriteLine(message);
+		}
+	}
+}
