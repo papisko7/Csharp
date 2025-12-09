27b2c6c5293e14ad74f1e01c057e8aaa680d7c3a
Papiśko <kkowalski0477@gmail.com>
2025-12-10 00:12:16 +0100

    Refactored the project's code.

--- Ultimate C# Masterclass/Section5/GameDataParser/GameDataParser.cs
+++ Ultimate C# Masterclass/Section5/GameDataParser/GameDataParser.cs
@@ -6,52 +6,15 @@ namespace GameDataParserNamespace
 	{
 		public void Run()
 		{
-			string fileContents = default;
-			string filePath = default;
-			bool isValidInput = false;
-			List<VideoGame> videoGames = default;
+			string? filePath = ReadFilePathFromUser();
+			string? fileContents = File.ReadAllText(filePath);
+			List<VideoGame> videoGames = DeserializeVideoGamesFrom(fileContents, filePath);
 
-			do
-			{
-				Console.WriteLine("Enter the file path you want to read (or file name if the file is in the project file): ");
-				filePath = Console.ReadLine();
+			PrintGames(videoGames);
+		}
 
-				if (filePath is null)
-				{
-					Console.WriteLine("File path cannot be null!");
-				}
-
-				else if (filePath.Equals(string.Empty))
-				{
-					Console.WriteLine("File path cannot be empty!");
-				}
-
-				else if (!File.Exists(filePath))
-				{
-					Console.WriteLine("File of a given path does not exist!");
-				}
-
-				fileContents = File.ReadAllText(filePath);
-				isValidInput = true;
-			} while (!isValidInput);
-
-			try
-			{
-				videoGames = JsonSerializer.Deserialize<List<VideoGame>>(fileContents);
-			}
-			catch (JsonException jex)
-			{
-				var originalColor = Console.ForegroundColor;
-
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine($"JSON in {filePath} file was not " +
-					"in a valid format. JSON body:");
-				Console.WriteLine(fileContents);
-				Console.ForegroundColor = originalColor;
-
-				throw new JsonException($"{jex.Message} The file is: {filePath}", jex);
-			}
-
+		private static void PrintGames(List<VideoGame> videoGames)
+		{
 			if (videoGames.Count > 0)
 			{
 				Console.WriteLine();
@@ -66,6 +29,56 @@ namespace GameDataParserNamespace
 			{
 				Console.WriteLine("No games are present in the input file.");
 			}
+		}
+
+		private List<VideoGame> DeserializeVideoGamesFrom(string fileContents, string filePath)
+		{
+			try
+			{
+				return JsonSerializer.Deserialize<List<VideoGame>>(fileContents) ?? new List<VideoGame>();
+			}
+			catch (JsonException jex)
+			{
+				var originalColor = Console.ForegroundColor;
+
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine($"JSON in {filePath} file was not " +
+					"in a valid format. JSON body:");
+				Console.WriteLine(fileContents);
+				Console.ForegroundColor = originalColor;
+
+				throw new JsonException($"{jex.Message} The file is: {filePath}", jex);
+			}
+		}
+
+		private string ReadFilePathFromUser()
+		{
+			var isFilePathValid = false;
+			var userFilePath = string.Empty;
+
+			do
+			{
+				Console.WriteLine("Enter the file path you want to read (or file name if the file is in the project file): ");
+				userFilePath = Console.ReadLine();
+
+				if (userFilePath is null)
+				{
+					Console.WriteLine("File path cannot be null!");
+				}
+
+				else if (userFilePath.Equals(string.Empty))
+				{
+					Console.WriteLine("File path cannot be empty!");
+				}
+
+				else if (!File.Exists(userFilePath))
+				{
+					Console.WriteLine("File of a given path does not exist!");
+				}
+				isFilePathValid = true;
+			} while (!isFilePathValid);
+
+			return userFilePath;
 		}
 	}
 }
