b8ce70683ab2c824c1883f20550cbbb5d1f4d3ce
Papiśko <kkowalski0477@gmail.com>
2025-12-09 22:43:25 +0100

    Implemented my own logger class to the project.

---
+++ Ultimate C# Masterclass/Section5/GameDataParser/Logger.cs
@@ -0,0 +1,20 @@
+﻿namespace LoggerNamespace
+{
+	public class Logger
+	{
+		private string _logFilePath;
+
+		public Logger(string logFilePath)
+		{
+			_logFilePath = logFilePath;
+		}
+
+		public void Log(Exception ex)
+		{
+			var entry = $@"[{DateTime.Now}]
+				Exception  message: {ex.Message}
+				Stack trace: {ex.StackTrace}";
+			File.AppendAllText(_logFilePath, entry);
+		}
+	}
+}
