27f24006991e10ea7857e054efaea47e3eb0fc5e
Papiśko <kkowalski0477@gmail.com>
2025-12-09 22:43:25 +0100

    Implemented my own logger class to the project.

---
+++ Ultimate C# Masterclass/Section5/GameDataParser/Main.cs
@@ -0,0 +1,15 @@
+﻿using GameDataParserNamespace;
+using LoggerNamespace;
+
+GameDataParser app = new GameDataParser();
+Logger logger = new Logger("log.txt");
+
+try
+{
+	app.Run();
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"An error occurred: {ex.Message}");
+	logger.Log(ex);
+}
