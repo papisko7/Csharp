ce06260e4a07e3d26d007959316bdbfcce8774b5
Papiśko <kkowalski0477@gmail.com>
2026-01-15 23:30:38 +0100

    Completed the assignment. Integrated decorator design pattern into the project. Followed Dependency inversion principle. Managed to implement a simple caching mechanism.

---
+++ Ultimate C# Masterclass/Section6/Section6/Logic/DataDownloader.cs
@@ -0,0 +1,26 @@
+﻿using Section6.Caching;
+using Section6.Interfaces;
+
+namespace Section6.Logic
+{
+	public class DataDownloader : IDataDownloader
+	{
+		private readonly IGenericCache<string, string> _cache;
+
+		public DataDownloader()
+		{
+			_cache = new GenericCache<string, string>();
+		}
+
+		public string DownloadData(string id)
+		{
+			Thread.Sleep(1000);
+			return $"Data for {id}";
+		}
+
+		public void DisplayData(string id)
+		{
+			Console.WriteLine(DownloadData(id));
+		}
+	}
+}
---
+++ Ultimate C# Masterclass/Section6/Section6/Program.cs
@@ -0,0 +1,28 @@
+﻿using Section6.Caching;
+using Section6.Interfaces;
+using Section6.Logic;
+
+namespace Section6
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			IDataDownloader dataDownloader = new CachingDataDownloader(new DataDownloader(), new GenericCache<string, string>());
+
+			dataDownloader.DisplayData("id1");
+			dataDownloader.DisplayData("id2");
+			dataDownloader.DisplayData("id1");
+			dataDownloader.DisplayData("id2");
+			dataDownloader.DisplayData("id1");
+			dataDownloader.DisplayData("id2");
+			dataDownloader.DisplayData("id1");
+			dataDownloader.DisplayData("id2");
+			dataDownloader.DisplayData("id1");
+			dataDownloader.DisplayData("id2");
+			dataDownloader.DisplayData("id3");
+			dataDownloader.DisplayData("id4");
+			dataDownloader.DisplayData("id5");
+		}
+	}
+}
