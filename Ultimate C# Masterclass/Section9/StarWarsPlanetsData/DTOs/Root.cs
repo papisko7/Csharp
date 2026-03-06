1ab9ece509e0c44e0979b128e7916e7a1aadde68
Papiśko <kkowalski0477@gmail.com>
2026-03-06 23:55:17 +0100

    Implemented a new simple console project, which allows the user to get data about 10 Star Wars Planets from swapi.dev API. Pre final version of the project as it needs further refactoring.

---
+++ Ultimate C# Masterclass/Section9/StarWarsPlanetsData/DTOs/Root.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+public record Root(
+		[property: JsonPropertyName("count")]
+			int? Count,
+
+		[property: JsonPropertyName("next")]
+			string Next,
+
+		[property: JsonPropertyName("previous")]
+			object Previous,
+
+		[property: JsonPropertyName("results")]
+			IReadOnlyList<Result> Results
+);
---
+++ Ultimate C# Masterclass/Section9/StarWarsPlanetsData/Program.cs
@@ -0,0 +1,17 @@
+﻿using StarWarsPlanetsData;
+using StarWarsPlanetsData.ApiDataAccess;
+
+try
+{
+	await new StarWarsPlanetsDataApp(
+		new ApiDataReader(),
+		new MockStarWarsApiDataReader()).Run();
+}
+catch (Exception ex)
+{
+	Console.WriteLine("An unexpected error occurred. " +
+		"Exception message: " + ex.Message);
+}
+
+Console.WriteLine("Press any key to close");
+Console.ReadKey();
