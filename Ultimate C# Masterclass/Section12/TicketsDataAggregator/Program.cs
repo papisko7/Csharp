55cbcd2329c2ba5555dd8407eb9cd194b9759171
kkowa <kkowa@EREDAN>
2026-05-07 21:57:14 +0200

    Created an initial boilerplate for this project. The app can read all the PDFs from the directory also it can distinguish between different cultures used in writing and creating those tickets.

---
+++ Ultimate C# Masterclass/Section12/TicketsDataAggregator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.TextExtractor;
+
+Console.WriteLine("Enter the directory path containing PDFs:");
+
+var ticketsDirectoryPath = Console.ReadLine();
+var pdfFiles = Directory.GetFiles(ticketsDirectoryPath, "*.pdf");
+List<string> aggregatedFileData = new List<string>();
+
+foreach (var filePath in pdfFiles)
+{
+    using PdfDocument document = PdfDocument.Open(filePath);
+
+    foreach (Page page in document.GetPages())
+    {
+        string text = ContentOrderTextExtractor.GetText(page);
+
+        CultureInfo culture = text.Contains(".com") ? new CultureInfo("en-US") :
+            text.Contains(".fr") ? new CultureInfo("fr-Fr") :
+            text.Contains(".jp") ? new CultureInfo("jp-JP") :
+            CultureInfo.InvariantCulture;
+    }
+}
