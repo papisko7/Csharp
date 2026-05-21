458e790b06990f1b3021840311cb39b36d0eddc9
papisko7 <kkowalski0477@gmail.com>
2026-05-21 18:04:46 +0200

    Created a basic Form1 structure for the app.

---
+++ Ultimate C# Masterclass/Section14/NumericTypesSuggester/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace NumericTypesSuggester
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			textBox1 = new TextBox();
+			textBox2 = new TextBox();
+			checkBox1 = new CheckBox();
+			checkBox2 = new CheckBox();
+			label1 = new Label();
+			SuspendLayout();
+			// 
+			// textBox1
+			// 
+			textBox1.Location = new Point(72, 12);
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(125, 27);
+			textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			textBox2.Location = new Point(72, 57);
+			textBox2.Name = "textBox2";
+			textBox2.Size = new Size(125, 27);
+			textBox2.TabIndex = 1;
+			// 
+			// checkBox1
+			// 
+			checkBox1.AutoSize = true;
+			checkBox1.Location = new Point(72, 100);
+			checkBox1.Name = "checkBox1";
+			checkBox1.Size = new Size(101, 24);
+			checkBox1.TabIndex = 2;
+			checkBox1.Text = "checkBox1";
+			checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			checkBox2.AutoSize = true;
+			checkBox2.Location = new Point(72, 133);
+			checkBox2.Name = "checkBox2";
+			checkBox2.Size = new Size(101, 24);
+			checkBox2.TabIndex = 3;
+			checkBox2.Text = "checkBox2";
+			checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(85, 180);
+			label1.Name = "label1";
+			label1.Size = new Size(50, 20);
+			label1.TabIndex = 4;
+			label1.Text = "label1";
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(label1);
+			Controls.Add(checkBox2);
+			Controls.Add(checkBox1);
+			Controls.Add(textBox2);
+			Controls.Add(textBox1);
+			Name = "Form1";
+			Text = "Form1";
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private TextBox textBox1;
+		private TextBox textBox2;
+		private CheckBox checkBox1;
+		private CheckBox checkBox2;
+		private Label label1;
+	}
+}
