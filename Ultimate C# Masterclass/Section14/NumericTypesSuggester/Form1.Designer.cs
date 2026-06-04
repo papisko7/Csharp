87aed06903880246444bf93301ff2830b9734a91
Papiśko <kkowalski0477@gmail.com>
2026-06-04 02:01:02 +0200

    Implemented logic for min and max values boxes and proper validations according to ducumentation.

--- Ultimate C# Masterclass/Section14/NumericTypesSuggester/Form1.Designer.cs
+++ Ultimate C# Masterclass/Section14/NumericTypesSuggester/Form1.Designer.cs
@@ -20,117 +20,121 @@
             base.Dispose(disposing);
         }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			minValTxtBx = new TextBox();
-			maxValueTxtBx = new TextBox();
-			integralChckBx = new CheckBox();
-			preciseChBx = new CheckBox();
-			typeSuggestionLbl = new Label();
-			maxValueLbl = new Label();
-			minValueLbl = new Label();
-			SuspendLayout();
-			// 
-			// minValTxtBx
-			// 
-			minValTxtBx.Location = new Point(175, 12);
-			minValTxtBx.Name = "minValTxtBx";
-			minValTxtBx.Size = new Size(579, 27);
-			minValTxtBx.TabIndex = 0;
-			// 
-			// maxValueTxtBx
-			// 
-			maxValueTxtBx.Location = new Point(175, 55);
-			maxValueTxtBx.Name = "maxValueTxtBx";
-			maxValueTxtBx.Size = new Size(579, 27);
-			maxValueTxtBx.TabIndex = 1;
-			// 
-			// integralChckBx
-			// 
-			integralChckBx.AutoSize = true;
-			integralChckBx.CheckAlign = ContentAlignment.MiddleRight;
-			integralChckBx.Checked = true;
-			integralChckBx.CheckState = CheckState.Checked;
-			integralChckBx.Location = new Point(72, 103);
-			integralChckBx.Name = "integralChckBx";
-			integralChckBx.Size = new Size(121, 24);
-			integralChckBx.TabIndex = 2;
-			integralChckBx.Text = "Integral only?";
-			integralChckBx.UseVisualStyleBackColor = true;
-			// 
-			// preciseChBx
-			// 
-			preciseChBx.AutoSize = true;
-			preciseChBx.CheckAlign = ContentAlignment.MiddleRight;
-			preciseChBx.Location = new Point(51, 133);
-			preciseChBx.Name = "preciseChBx";
-			preciseChBx.Size = new Size(142, 24);
-			preciseChBx.TabIndex = 3;
-			preciseChBx.Text = "Must be precise?";
-			preciseChBx.UseVisualStyleBackColor = true;
-			preciseChBx.Visible = false;
-			// 
-			// typeSuggestionLbl
-			// 
-			typeSuggestionLbl.AutoSize = true;
-			typeSuggestionLbl.BackColor = SystemColors.ActiveCaption;
-			typeSuggestionLbl.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-			typeSuggestionLbl.Location = new Point(31, 174);
-			typeSuggestionLbl.Name = "typeSuggestionLbl";
-			typeSuggestionLbl.Size = new Size(241, 20);
-			typeSuggestionLbl.TabIndex = 4;
-			typeSuggestionLbl.Text = "Suggested type: not enough data";
-			// 
-			// maxValueLbl
-			// 
-			maxValueLbl.AutoSize = true;
-			maxValueLbl.Location = new Point(89, 62);
-			maxValueLbl.Name = "maxValueLbl";
-			maxValueLbl.Size = new Size(80, 20);
-			maxValueLbl.TabIndex = 5;
-			maxValueLbl.Text = "Max Value:";
-			// 
-			// minValueLbl
-			// 
-			minValueLbl.AutoSize = true;
-			minValueLbl.Location = new Point(92, 19);
-			minValueLbl.Name = "minValueLbl";
-			minValueLbl.Size = new Size(77, 20);
-			minValueLbl.TabIndex = 6;
-			minValueLbl.Text = "Min Value:";
-			// 
-			// Form1
-			// 
-			AutoScaleDimensions = new SizeF(8F, 20F);
-			AutoScaleMode = AutoScaleMode.Font;
-			BackColor = SystemColors.ActiveCaption;
-			ClientSize = new Size(800, 217);
-			Controls.Add(minValueLbl);
-			Controls.Add(maxValueLbl);
-			Controls.Add(typeSuggestionLbl);
-			Controls.Add(preciseChBx);
-			Controls.Add(integralChckBx);
-			Controls.Add(maxValueTxtBx);
-			Controls.Add(minValTxtBx);
-			Cursor = Cursors.Default;
-			ForeColor = SystemColors.ControlText;
-			FormBorderStyle = FormBorderStyle.FixedSingle;
-			Name = "Form1";
-			Text = "C# Numeric Types";
-			Load += Form1_Load;
-			ResumeLayout(false);
-			PerformLayout();
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            minValueTxtBx = new TextBox();
+            maxValueTxtBx = new TextBox();
+            integralChckBx = new CheckBox();
+            preciseChBx = new CheckBox();
+            typeSuggestionLbl = new Label();
+            maxValueLbl = new Label();
+            minValueLbl = new Label();
+            SuspendLayout();
+            // 
+            // minValueTxtBx
+            // 
+            minValueTxtBx.Location = new Point(153, 9);
+            minValueTxtBx.Margin = new Padding(3, 2, 3, 2);
+            minValueTxtBx.Name = "minValueTxtBx";
+            minValueTxtBx.Size = new Size(507, 23);
+            minValueTxtBx.TabIndex = 0;
+            // 
+            // maxValueTxtBx
+            // 
+            maxValueTxtBx.Location = new Point(153, 41);
+            maxValueTxtBx.Margin = new Padding(3, 2, 3, 2);
+            maxValueTxtBx.Name = "maxValueTxtBx";
+            maxValueTxtBx.Size = new Size(507, 23);
+            maxValueTxtBx.TabIndex = 1;
+            // 
+            // integralChckBx
+            // 
+            integralChckBx.AutoSize = true;
+            integralChckBx.CheckAlign = ContentAlignment.MiddleRight;
+            integralChckBx.Checked = true;
+            integralChckBx.CheckState = CheckState.Checked;
+            integralChckBx.Location = new Point(63, 77);
+            integralChckBx.Margin = new Padding(3, 2, 3, 2);
+            integralChckBx.Name = "integralChckBx";
+            integralChckBx.Size = new Size(97, 19);
+            integralChckBx.TabIndex = 2;
+            integralChckBx.Text = "Integral only?";
+            integralChckBx.UseVisualStyleBackColor = true;
+            // 
+            // preciseChBx
+            // 
+            preciseChBx.AutoSize = true;
+            preciseChBx.CheckAlign = ContentAlignment.MiddleRight;
+            preciseChBx.Location = new Point(45, 100);
+            preciseChBx.Margin = new Padding(3, 2, 3, 2);
+            preciseChBx.Name = "preciseChBx";
+            preciseChBx.Size = new Size(114, 19);
+            preciseChBx.TabIndex = 3;
+            preciseChBx.Text = "Must be precise?";
+            preciseChBx.UseVisualStyleBackColor = true;
+            preciseChBx.Visible = false;
+            // 
+            // typeSuggestionLbl
+            // 
+            typeSuggestionLbl.AutoSize = true;
+            typeSuggestionLbl.BackColor = SystemColors.ActiveCaption;
+            typeSuggestionLbl.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            typeSuggestionLbl.Location = new Point(27, 130);
+            typeSuggestionLbl.Name = "typeSuggestionLbl";
+            typeSuggestionLbl.Size = new Size(191, 15);
+            typeSuggestionLbl.TabIndex = 4;
+            typeSuggestionLbl.Text = "Suggested type: not enough data";
+            // 
+            // maxValueLbl
+            // 
+            maxValueLbl.AutoSize = true;
+            maxValueLbl.Location = new Point(78, 46);
+            maxValueLbl.Name = "maxValueLbl";
+            maxValueLbl.Size = new Size(63, 15);
+            maxValueLbl.TabIndex = 5;
+            maxValueLbl.Text = "Max Value:";
+            // 
+            // minValueLbl
+            // 
+            minValueLbl.AutoSize = true;
+            minValueLbl.Location = new Point(80, 14);
+            minValueLbl.Name = "minValueLbl";
+            minValueLbl.Size = new Size(62, 15);
+            minValueLbl.TabIndex = 6;
+            minValueLbl.Text = "Min Value:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(700, 163);
+            Controls.Add(minValueLbl);
+            Controls.Add(maxValueLbl);
+            Controls.Add(typeSuggestionLbl);
+            Controls.Add(preciseChBx);
+            Controls.Add(integralChckBx);
+            Controls.Add(maxValueTxtBx);
+            Controls.Add(minValueTxtBx);
+            ForeColor = SystemColors.ControlText;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "C# Numeric Types";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
+        #endregion
 
-		private TextBox minValTxtBx;
+        private TextBox minValueTxtBx;
 		private TextBox maxValueTxtBx;
 		private CheckBox integralChckBx;
 		private CheckBox preciseChBx;
--- Ultimate C# Masterclass/Section14/NumericTypesSuggester/Form1.cs
+++ Ultimate C# Masterclass/Section14/NumericTypesSuggester/Form1.cs
@@ -1,15 +1,83 @@
-namespace NumericTypesSuggester
+using System.Numerics;
+
+namespace NumericTypesSuggester;
+
+public partial class Form1 : Form
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
+    public Form1()
+    {
+        InitializeComponent();
 
-		private void Form1_Load(object sender, EventArgs e)
-		{
+        minValueTxtBx.KeyPress += TextBox_KeyPress;
+        maxValueTxtBx.KeyPress += TextBox_KeyPress;
 
-		}
-	}
+        minValueTxtBx.TextChanged += TextBox_TextChanged;
+        maxValueTxtBx.TextChanged += TextBox_TextChanged;
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        RecalculateSyggestion();
+    }
+
+    private void TextBox_TextChanged(object sender, EventArgs e)
+    {
+        RecalculateSyggestion();
+    }
+
+    private void RecalculateSyggestion()
+    {
+        var minText = minValueTxtBx.Text;
+        var maxText = maxValueTxtBx.Text;
+
+        maxValueTxtBx.BackColor = Color.White;
+
+        ValidateMinMaxText(minText, maxText);
+        ParseMinMaxTextToBigInt(minText, maxText);
+    }
+
+    private void ValidateMinMaxText(string minText, string maxText)
+    {
+        if (string.IsNullOrWhiteSpace(minText) || minText.Equals("-")
+            || string.IsNullOrWhiteSpace(maxText) || maxText.Equals("-"))
+        {
+            typeSuggestionLbl.Text = @"Suggested type: not enough data";
+            return;
+        }
+
+        return;
+    }
+
+    private void ParseMinMaxTextToBigInt(string minText, string maxText)
+    {
+        if (BigInteger.TryParse(minText, out BigInteger minValue) &&
+            BigInteger.TryParse(maxText, out BigInteger maxValue))
+        {
+            if (maxValue < minValue)
+            {
+                maxValueTxtBx.BackColor = Color.Red;
+                typeSuggestionLbl.Text = "Suggested type: not enough data";
+            }
+            else
+            {
+                typeSuggestionLbl.Text = "Suggested type: [Gotowe na algorytm]";
+            }
+        }
+    }
+
+    private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        TextBox textBox = (TextBox)sender;
+        if (char.IsDigit(e.KeyChar) || e.KeyChar == (char)Keys.Back)
+        {
+            return;
+        }
+
+        if (e.KeyChar == '-' && textBox.SelectionStart == 0 && !textBox.Text.Contains("-"))
+        {
+            return;
+        }
+
+        e.Handled = true;
+    }
 }
