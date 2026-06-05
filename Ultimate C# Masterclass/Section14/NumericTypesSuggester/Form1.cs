9be38f2f1c63e5bb1d6cee8983a8e42ba6312c9a
kkowa <kkowa@EREDAN>
2026-06-05 13:22:13 +0200

    Refactored the code to be inline with programming principles. Added a resource file.

--- Ultimate C# Masterclass/Section14/NumericTypesSuggester/Form1.cs
+++ Ultimate C# Masterclass/Section14/NumericTypesSuggester/Form1.cs
@@ -1,185 +1,106 @@
 using System.Numerics;
 
-namespace NumericTypesSuggester;
-
-public partial class Form1 : Form
+namespace NumericTypesSuggester
 {
-	public Form1()
+	public partial class Form1 : Form
 	{
-		InitializeComponent();
-
-		minValueTxtBx.KeyPress += TextBox_KeyPress;
-		maxValueTxtBx.KeyPress += TextBox_KeyPress;
-
-		minValueTxtBx.TextChanged += TextBox_TextChanged;
-		maxValueTxtBx.TextChanged += TextBox_TextChanged;
-
-		integralChckBx.CheckedChanged += IntegralChckBx_CheckedChanged;
-		preciseChBx.CheckedChanged += PreciseChBx_CheckedChanged;
-	}
-
-	private void Form1_Load(object sender, EventArgs e)
-	{
-		RecalculateSuggestion();
-	}
-
-	private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
-	{
-		TextBox textBox = (TextBox)sender;
-		if (char.IsDigit(e.KeyChar) || e.KeyChar == (char)Keys.Back)
+		public Form1()
 		{
-			return;
+			InitializeComponent();
+			RegisterEvents();
 		}
 
-		if (e.KeyChar == '-' && textBox.SelectionStart == 0 && !textBox.Text.Contains("-"))
+		private void Form1_Load(object sender, EventArgs e)
 		{
-			return;
+			RecalculateSuggestion();
 		}
 
-		e.Handled = true;
-	}
-
-	private void TextBox_TextChanged(object sender, EventArgs e)
-	{
-		RecalculateSuggestion();
-	}
-
-	private void IntegralChckBx_CheckedChanged(object sender, EventArgs e)
-	{
-		preciseChBx.Visible = !integralChckBx.Checked;
-
-		RecalculateSuggestion();
-	}
-
-	private void PreciseChBx_CheckedChanged(object sender, EventArgs e)
-	{
-		RecalculateSuggestion();
-	}
-
-	private void RecalculateSuggestion()
-	{
-		var minText = minValueTxtBx.Text;
-		var maxText = maxValueTxtBx.Text;
-
-		maxValueTxtBx.BackColor = Color.White;
-
-		ValidateMinMaxText(minText, maxText);
-		ParseMinMaxTextToBigInt(minText, maxText);
-	}
-
-	private void ValidateMinMaxText(string minText, string maxText)
-	{
-		if (string.IsNullOrWhiteSpace(minText) || minText.Equals("-")
-			|| string.IsNullOrWhiteSpace(maxText) || maxText.Equals("-"))
+		private void RegisterEvents()
 		{
-			typeSuggestionLbl.Text = @"Suggested type: not enough data";
+			minValueTxtBx.KeyPress += TextBox_KeyPress;
+			maxValueTxtBx.KeyPress += TextBox_KeyPress;
+
+			minValueTxtBx.TextChanged += TextBox_TextChanged;
+			maxValueTxtBx.TextChanged += TextBox_TextChanged;
+
+			integralChckBx.CheckedChanged += IntegralChckBx_CheckedChanged;
+			preciseChBx.CheckedChanged += PreciseChBx_CheckedChanged;
 		}
 
-		return;
-	}
-
-	private void ParseMinMaxTextToBigInt(string minText, string maxText)
-	{
-		if (BigInteger.TryParse(minText, out BigInteger minValue) &&
-			BigInteger.TryParse(maxText, out BigInteger maxValue))
+		private void TextBox_KeyPress(object? sender, KeyPressEventArgs e)
 		{
-			if (maxValue < minValue)
+			if (sender is not TextBox textBox)
 			{
-				maxValueTxtBx.BackColor = Color.Red;
-				typeSuggestionLbl.Text = "Wrong input! Max value has to be greater than min value";
+				return;
 			}
-			else
+
+			if (char.IsDigit(e.KeyChar) || e.KeyChar == (char)Keys.Back)
 			{
-				string suggestedType;
-				if (integralChckBx.Checked)
+				return;
+			}
+
+			if (e.KeyChar == '-' && textBox.SelectionStart == 0 && !textBox.Text.Contains("-"))
+			{
+				return;
+			}
+
+			e.Handled = true;
+		}
+
+		private void TextBox_TextChanged(object? sender, EventArgs e)
+		{
+			RecalculateSuggestion();
+		}
+
+		private void IntegralChckBx_CheckedChanged(object? sender, EventArgs e)
+		{
+			preciseChBx.Visible = !integralChckBx.Checked;
+			RecalculateSuggestion();
+		}
+
+		private void PreciseChBx_CheckedChanged(object? sender, EventArgs e)
+		{
+			RecalculateSuggestion();
+		}
+
+		private void RecalculateSuggestion()
+		{
+			string minText = minValueTxtBx.Text;
+			string maxText = maxValueTxtBx.Text;
+
+			maxValueTxtBx.BackColor = Color.White;
+			if (IsInputInvalid(minText) || IsInputInvalid(maxText))
+			{
+				typeSuggestionLbl.Text = AppStrings.NotEnoughData;
+				return;
+			}
+
+			if (BigInteger.TryParse(minText, out BigInteger minValue) &&
+				BigInteger.TryParse(maxText, out BigInteger maxValue))
+			{
+				if (maxValue < minValue)
 				{
-					suggestedType = GetSuggestedIntegralType(minValue, maxValue);
-				}
-				else
-				{
-					suggestedType = GetSuggestedFloatingPointType(minValue, maxValue, preciseChBx.Checked);
+					maxValueTxtBx.BackColor = Color.Red;
+					typeSuggestionLbl.Text = AppStrings.NotEnoughData;
+
+					return;
 				}
 
-				typeSuggestionLbl.Text = $"Suggested type: {suggestedType}";
+				string resultType = TypeSuggester.Suggest(
+					minValue,
+					maxValue,
+					integralChckBx.Checked,
+					preciseChBx.Checked
+				);
+
+				typeSuggestionLbl.Text = string.Format(AppStrings.SuggestedTypeFormat, resultType);
 			}
 		}
-	}
 
-	private string GetSuggestedIntegralType(BigInteger minValue, BigInteger maxValue)
-	{
-		if (minValue >= 0)
+		private bool IsInputInvalid(string text)
 		{
-			if (maxValue <= byte.MaxValue)
-			{
-				return "byte";
-			}
-
-			if (maxValue <= ushort.MaxValue)
-			{
-				return "ushort";
-			}
-
-			if (maxValue <= uint.MaxValue)
-			{
-				return "uint";
-			}
-
-			if (maxValue <= ulong.MaxValue)
-			{
-				return "ulong";
-			}
-
-			return "BigInteger";
-		}
-		else
-		{
-			if (minValue >= sbyte.MinValue && maxValue <= sbyte.MaxValue)
-			{
-				return "sbyte";
-			}
-
-			if (minValue >= short.MinValue && maxValue <= short.MaxValue)
-			{
-				return "short";
-			}
-
-			if (minValue >= int.MinValue && maxValue <= int.MaxValue)
-			{
-				return "int";
-			}
-
-			if (minValue >= long.MinValue && maxValue <= long.MaxValue)
-			{
-				return "long";
-			}
-
-			return "BigInteger";
-		}
-	}
-
-	private string GetSuggestedFloatingPointType(BigInteger minValue, BigInteger maxValue, bool mustBePercise)
-	{
-		if (mustBePercise)
-		{
-			if (minValue >= (BigInteger)decimal.MinValue && maxValue <= (BigInteger)decimal.MaxValue)
-			{
-				return "decimal";
-			}
-
-			return "Impossible representation";
-		}
-		else
-		{
-			if (minValue >= (BigInteger)float.MinValue && maxValue <= (BigInteger)float.MaxValue)
-			{
-				return "float";
-			}
-			if (minValue >= (BigInteger)double.MinValue && maxValue <= (BigInteger)double.MaxValue)
-			{
-				return "double";
-			}
-
-			return "Impossible representation";
+			return string.IsNullOrWhiteSpace(text)
+				|| text == "-";
 		}
 	}
 }
---
+++ Ultimate C# Masterclass/Section14/NumericTypesSuggester/TypeSuggester.cs
@@ -0,0 +1,102 @@
+﻿using System.Numerics;
+
+namespace NumericTypesSuggester
+{
+	public static class TypeSuggester
+	{
+		public static string Suggest(BigInteger min, BigInteger max,
+			bool isIntegral, bool mustBePrecise)
+		{
+			if (isIntegral)
+			{
+				return SuggestIntegral(min, max);
+			}
+
+			return SuggestFloatingPoint(min, max, mustBePrecise);
+		}
+
+		private static string SuggestIntegral(BigInteger min, BigInteger max)
+		{
+			if (min >= 0)
+			{
+				if (max <= byte.MaxValue)
+				{
+					return "byte";
+				}
+
+				if (max <= ushort.MaxValue)
+				{
+					return "ushort";
+				}
+
+				if (max <= uint.MaxValue)
+				{
+					return "uint";
+				}
+
+				if (max <= ulong.MaxValue)
+				{
+					return "ulong";
+				}
+
+				return "BigInteger";
+			}
+
+			if (min >= sbyte.MinValue && max <= sbyte.MaxValue)
+			{
+				return "sbyte";
+			}
+
+			if (min >= short.MinValue && max <= short.MaxValue)
+			{
+				return "short";
+			}
+
+			if (min >= int.MinValue && max <= int.MaxValue)
+			{
+				return "int";
+			}
+
+			if (min >= long.MinValue && max <= long.MaxValue)
+			{
+				return "long";
+			}
+
+			return "BigInteger";
+		}
+
+		private static string SuggestFloatingPoint(BigInteger min, BigInteger max, bool mustBePrecise)
+		{
+			if (mustBePrecise)
+			{
+				var decimalMin = new BigInteger(decimal.MinValue);
+				var decimalMax = new BigInteger(decimal.MaxValue);
+
+				if (min >= decimalMin && max <= decimalMax)
+				{
+					return "decimal";
+				}
+
+				return AppStrings.ImpossibleRepresentation;
+			}
+
+			var minAsDouble = (double)min;
+			var maxAsDouble = (double)max;
+
+			if (double.IsInfinity(minAsDouble) || double.IsInfinity(maxAsDouble))
+			{
+				return AppStrings.ImpossibleRepresentation;
+			}
+
+			var minAsFloat = (float)min;
+			var maxAsFloat = (float)max;
+
+			if (!float.IsInfinity(minAsFloat) && !float.IsInfinity(maxAsFloat))
+			{
+				return "float";
+			}
+
+			return "double";
+		}
+	}
+}
