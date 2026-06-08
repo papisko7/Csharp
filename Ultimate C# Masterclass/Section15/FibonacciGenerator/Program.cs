0507129286af1bb63a28fa4cebb040d667064ea6
kkowa <kkowa@EREDAN>
2026-06-08 14:22:18 +0200

    Completed project setup.

---
+++ Ultimate C# Masterclass/Section15/FibonacciGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿using FibonacciGenerator;
+
+var fibbonaciSequence = Fibonacci.Generate(7);
+
+Console.Write('[');
+foreach (var element in fibbonaciSequence)
+{
+	Console.Write(element + " ");
+}
+
+Console.Write(']');
