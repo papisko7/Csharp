5396b134b4dc72aa2e7728e4ca1480e9e5f0684f
kkowa <kkowa@EREDAN>
2026-07-15 23:50:14 +0200

    Started working on the initial implementation of the assignment. Phase 1 of the application workflow in progress.

---
+++ Ultimate C# Masterclass/Section17/QuoteFinder/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter the word you want to search: ");
+var searchedWord = Console.ReadLine();
+
+if (int.TryParse(searchedWord, out var word)
+	|| string.IsNullOrWhiteSpace(searchedWord)
+	|| WordHasSpecialCharacters(searchedWord))
+{
+	Console.WriteLine("The searched word has to be a single word only it cannot be a sentence, nor have any special characters (includes numbers). The word has to be provided and cannot be a whitespace.");
+}
+
+static bool WordHasSpecialCharacters(string word)
+{
+	return word.Any(character => !char.IsLetterOrDigit(character));
+}
