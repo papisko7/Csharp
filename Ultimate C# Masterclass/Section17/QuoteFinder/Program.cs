e9053b052c4d89cbbbe0d2f9a6dc0dcda8ca4e43
kkowa <kkowa@EREDAN>
2026-07-21 18:32:18 +0200

    Created Dtos from API response.

--- Ultimate C# Masterclass/Section17/QuoteFinder/Program.cs
+++ Ultimate C# Masterclass/Section17/QuoteFinder/Program.cs
@@ -1,4 +1,6 @@
-﻿const int PARSING_FAILURE = -1;
+﻿using Newtonsoft.Json;
+
+const int PARSING_FAILURE = -1;
 
 Console.WriteLine("Enter the word you want to search: ");
 var searchedWord = Console.ReadLine();
@@ -15,6 +17,8 @@ var parsedNumberOfQuotes = ParseToInt(numberOfQuotes);
 
 Console.WriteLine("Enable parallel processing? (y/n): ");
 var shouldEnableParallelProcessing = Console.ReadLine();
+
+// var httpClient = new HttpClient();
 
 Console.WriteLine("Program is finished.");
 
@@ -49,3 +53,48 @@ static int ParseToInt(string? parsedString)
 
 	return result;
 }
+
+public record Datum(
+	[property: JsonProperty("_id")]
+	string Id,
+
+	[property: JsonProperty("quoteText")]
+	string QuoteText,
+
+	[property: JsonProperty("quoteAuthor")]
+	string QuoteAuthor,
+
+	[property: JsonProperty("quoteGenre")]
+	string QuoteGenre,
+
+	[property: JsonProperty("__v")]
+	int? V
+);
+
+public record Pagination(
+	[property: JsonProperty("currentPage")]
+	int? CurrentPage,
+
+	[property: JsonProperty("nextPage")]
+	int? NextPage,
+
+	[property: JsonProperty("totalPages")]
+	int? TotalPages
+);
+
+public record Root(
+	[property: JsonProperty("statusCode")]
+	int? StatusCode,
+
+	[property: JsonProperty("message")]
+	string Message,
+
+	[property: JsonProperty("pagination")]
+	Pagination Pagination,
+
+	[property: JsonProperty("totalQuotes")]
+	int? TotalQuotes,
+
+	[property: JsonProperty("data")]
+	IReadOnlyList<Datum> Data
+);
